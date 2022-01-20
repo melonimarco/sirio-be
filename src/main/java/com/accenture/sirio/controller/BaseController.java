176962862a9ce7bd7176c93b42376304c3638420
valerio-inconis <valerio.inconis@accenture.com>
2022-01-20 11:58:14 +0100

    #1 - aggiunto response entity e fix path

--- src/main/java/com/accenture/sirio/controller/BaseController.java
+++ src/main/java/com/accenture/sirio/controller/BaseController.java
@@ -1,6 +1,8 @@
 package com.accenture.sirio.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,10 +18,10 @@ import com.accenture.sirio.facade.TipoEntitaInserimentoFacade;
 public class BaseController {
 	@Autowired
 	private TipoEntitaInserimentoFacade tipoEntitaInserimentoFacade;
-
+	
 	@GetMapping(path="/getInitCreazione")
-	public @ResponseBody Iterable<TipoEntitaInserimentoTO> getgetInitCreazione(){
-		return tipoEntitaInserimentoFacade.findAll();
+	public ResponseEntity<Object> getgetInitCreazione(){
+		return new ResponseEntity<>(tipoEntitaInserimentoFacade.findAll(), HttpStatus.OK);
 	}
 
 
